5af6a6ff9cbb953d1067ff1afd8e5da80bf23d7a
Mike Miller <michael.miller@banno.com>
2014-11-10 20:36:30 -0600

    added listeners to coming in list

---
+++ UI/src/Inventory/ExpandedComingInView.java
@@ -0,0 +1,66 @@
+package Inventory;
+
+import java.awt.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.swing.*;
+
+public class ExpandedComingInView extends JFrame{
+  CheckoutItem firstItem;
+  ExpandedComingInView frame;
+  JPanel itemPane;
+  
+  public ExpandedComingInView(List<CheckoutItem> items) {
+	frame = this;
+    frame.setSize(new Dimension(250,350));
+
+	JPanel panel = new JPanel();
+	firstItem = items.get(0);
+    panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+    JPanel studentIDPanel = new JPanel();
+    studentIDPanel.setLayout(new GridLayout(1,2));
+    studentIDPanel.add(new JLabel("Student Id :"));
+    studentIDPanel.add(new JLabel(firstItem.getStudentId()));
+    studentIDPanel.setMaximumSize(new Dimension(2000,25));
+    
+    JPanel studentEmailPanel = new JPanel();
+    studentEmailPanel.setLayout(new GridLayout(1,2));
+    studentEmailPanel.add(new JLabel("Student Email :"));
+    studentEmailPanel.add(new JLabel(firstItem.getStudentEmail()));
+    studentEmailPanel.setMaximumSize(new Dimension(2000,25));
+
+
+    DateFormat format = new SimpleDateFormat( "h:mm a" );	
+    JPanel timePanel = new JPanel();
+    timePanel.setLayout(new GridLayout(1,2));
+    timePanel.add(new JLabel("Time :"));
+    timePanel.add(new JLabel(format.format(firstItem.dueDate)));
+    timePanel.setMaximumSize(new Dimension(2000,25));
+
+	panel.add(studentIDPanel);
+	panel.add(studentEmailPanel);
+	panel.add(timePanel);
+
+    itemPane = new JPanel();
+	itemPane.setLayout(new GridLayout(1,2));
+	itemPane.add(new JLabel("Item Id"));
+	itemPane.add(new JLabel("Discription"));
+	itemPane.setMaximumSize(new Dimension(2000,25));
+	panel.add(itemPane);
+    for (CheckoutItem item : items) {
+      itemPane = new JPanel();
+	  itemPane.setLayout(new GridLayout(1,2));
+	  itemPane.add(new JLabel(item.getItemId()));
+	  itemPane.add(new JLabel(item.getItemDiscription()));
+	  itemPane.setMaximumSize(new Dimension(2000,25));
+
+	  panel.add(itemPane);
+    }
+    panel.setVisible(true);
+    frame.add(panel);
+    frame.setVisible(true);
+  }
+}
--- UI/src/Inventory/OverviewComingInItem.java
+++ UI/src/Inventory/OverviewComingInItem.java
@@ -1,11 +1,14 @@
 package Inventory;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -17,10 +20,13 @@ public class OverviewComingInItem extends JPanel {
   OverviewComingInItem panel;
   JLabel due;
   CheckoutItem current;
+  Color defaultColor;
+  ExpandedComingInView j;
   List<CheckoutItem> allItems;
   
   public OverviewComingInItem(List<CheckoutItem> items) {
 	panel = this;
+	defaultColor = panel.getBackground();
 	current = items.get(0);
 	allItems = items;
 	panel.setLayout(new GridLayout(1, 2));
@@ -30,10 +36,45 @@ public class OverviewComingInItem extends JPanel {
 	
 	panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
 	panel.setMaximumSize(new Dimension(2000,50));
-	
 	panel.add(new JLabel(current.getStudentEmail()));
 	panel.add(due);
+	panel.addMouseListener(new Mouse());
 	panel.setVisible(true);
+  }
+  
+  private class Mouse implements MouseListener {
 
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+	    System mainWindow = ((System) panel.getParent().getParent().getParent().getParent().getParent().getParent());
+	    JTabbedPane tabedPane = ((JTabbedPane) panel.getParent().getParent().getParent());
+	    mainWindow.checkinTab().overviewPanelClicked(current.studentId);
+	    tabedPane.setSelectedIndex(2);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	    panel.setBackground(new Color(75,17,111));
+	    j = new ExpandedComingInView(allItems);
+	    j.setLocation((int) Component.RIGHT_ALIGNMENT, (int) Component.TOP_ALIGNMENT);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	    panel.setBackground(defaultColor);
+	    j.dispose();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+	}
   }
 }
