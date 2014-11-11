026a6a906246222d98bafe1ae97cc34bd88c09f0
Mike Miller <michael.miller@banno.com>
2014-11-11 13:08:09 -0600

    added expanded view for missing item

--- UI/src/Inventory/OverviewMissingItem.java
+++ UI/src/Inventory/OverviewMissingItem.java
@@ -1,11 +1,14 @@
 package Inventory;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,13 +16,17 @@ import javax.swing.*;
 
 public class OverviewMissingItem extends JPanel {
   OverviewMissingItem panel;
+  ExpandedMissingItemView expanded;
   JLabel itemDiscription;
   JLabel itemIdLabel;
   InventoryItem current;
-  
+  Color defaultColor;
+
   public OverviewMissingItem(InventoryItem item) {
 	panel = this;
 	current = item;
+	defaultColor = panel.getBackground();
+
 	panel.setLayout(new GridLayout(1, 2));
 	
 	panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
@@ -27,6 +34,34 @@ public class OverviewMissingItem extends JPanel {
 	
 	panel.add(new JLabel(current.getId()));
 	panel.add(new JLabel(current.getDiscription()));
+	panel.addMouseListener(new Mouse());
 	panel.setVisible(true);
   }
+  private class Mouse implements MouseListener {
+
+    @Override
+	public void mouseClicked(MouseEvent arg0) {
+//	  System mainWindow = ((System) panel.getParent().getParent().getParent().getParent().getParent().getParent());
+//	  JTabbedPane tabedPane = ((JTabbedPane) panel.getParent().getParent().getParent());
+    }
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	  panel.setBackground(new Color(75,17,111));
+	  expanded = new ExpandedMissingItemView(current);
+	  expanded.setLocation((int) Component.RIGHT_ALIGNMENT, (int) Component.TOP_ALIGNMENT);
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	  panel.setBackground(defaultColor);
+	  expanded.dispose();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {}
+	}
 }
