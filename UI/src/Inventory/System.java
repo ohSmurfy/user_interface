5b02ebff054c7e7e41d7ca3c319c5bfc8c16ba75
samfisher3 <sam.fisher3@banno.com>
2014-12-07 15:58:22 -0600

    Fixed System

--- UI/src/Inventory/System.java
+++ UI/src/Inventory/System.java
@@ -5,6 +5,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 import java.awt.*;
+import java.text.ParseException;
  
 public class System extends JPanel {
   JTabbedPane tabbedPane;
@@ -16,23 +17,24 @@ public class System extends JPanel {
   public System() {
     panel = this;
     panel.setLayout(new GridLayout(1,1));
-//    APIReference a = new APIReference();
-//    a.getReservations();
+    APIReference a = new APIReference();
     tabbedPane = new JTabbedPane();
     checkIn = new CheckInTabPanel();
     inventory = new InventoryTabPanel();
     overview = new OverviewTabPanel();
     checkout = new CheckoutPanel();
-    
-    JScrollPane scrollCheckIn = new JScrollPane(checkIn);
-    JScrollPane scrollInventory = new JScrollPane(inventory);
-    JScrollPane scrollOverview = new JScrollPane(overview);
-    JScrollPane scrollCheckout = new JScrollPane(checkout);
-
-    tabbedPane.addTab("Overview", scrollOverview);
-    tabbedPane.addTab("Check out", scrollCheckout);
-    tabbedPane.addTab("Check in", scrollCheckIn);
-    tabbedPane.addTab("Inventory", scrollInventory);
+    try
+    {
+    	a.getReservations();
+    }
+    catch(ParseException p1)
+    {
+    	java.lang.System.out.println("Broke");
+    }
+    tabbedPane.addTab("Overview", overview);
+    tabbedPane.addTab("Check out", checkout);
+    tabbedPane.addTab("Check in", checkIn);
+    tabbedPane.addTab("Inventory", inventory);
     tabbedPane.addChangeListener(new ChangeListener() {
 	  public void stateChanged(ChangeEvent arg0) {
 		(inventory.getListPanel()).refresh();
@@ -40,9 +42,8 @@ public class System extends JPanel {
 		checkout.refreshCheckouttab();
 		}
       });
-    panel.add(tabbedPane);
-    panel.setBackground(Color.DARK_GRAY);
-
+    JScrollPane scroll = new JScrollPane(tabbedPane);
+    panel.add(scroll);     
   }
   
   public CheckInTabPanel checkinTab(){
